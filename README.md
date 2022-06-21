5047e2f0f3a0d5dbb701bb23fa0e682486a43eca
Joey Lim <jyorin0206@gmail.com>
2022-06-21 23:42:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# credentity
+
+A digital wallet to centralise your credentials and identity.
---
+++ android/app/src/main/kotlin/com/example/credentity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.credentity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
