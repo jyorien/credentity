a56200cac33cbda7d4f04b78a5ae1e301a9b3e99
Zechariah Tan <zechariahtan144@gmail.com>
2022-06-22 16:30:52 +0800

    ✨ change app swatch color

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,18 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: "Credentity",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: const MaterialColor(0x00434DFF, {
+          50: Color.fromRGBO(26, 30, 100, 0.1),
+          100: Color.fromRGBO(26, 30, 100, 0.2),
+          200: Color.fromRGBO(26, 30, 100, 0.3),
+          300: Color.fromRGBO(26, 30, 100, 0.4),
+          400: Color.fromRGBO(26, 30, 100, 0.5),
+          500: Color.fromRGBO(26, 30, 100, 0.6),
+          600: Color.fromRGBO(26, 30, 100, 0.7),
+          700: Color.fromRGBO(26, 30, 100, 0.8),
+          800: Color.fromRGBO(26, 30, 100, 0.9),
+          900: Color.fromRGBO(26, 30, 100, 1)
+        }),
         fontFamily: "SFProText",
       ),
       home: Scaffold(
