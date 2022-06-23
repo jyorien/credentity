6bac609402f9577d394d69594a1baa7da8f241c0
Zechariah <zechariahtan144@gmail.com>
2022-06-24 05:10:15 +0800

    ✨ hopefully I initialized firebase correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:credentity/features/qrcode/qrcode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
 
