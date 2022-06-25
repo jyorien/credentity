7ca5272838ed485cd6d5c06a150b78120248add2
Joey Lim <jyorin0206@gmail.com>
2022-06-25 20:06:36 +0800

    refactored input boxes

---
+++ lib/features/ocr/constants.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+TextFormField customTextFormField(
+    {required String labelText, required TextEditingController controller}) {
+  return TextFormField(
+    controller: controller,
+    decoration: InputDecoration(
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.cancel_outlined),
+        onPressed: () {
+          controller.text = "";
+        },
+      ),
+      labelText: labelText,
+      labelStyle: const TextStyle(color: Colors.black),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: const BorderSide(
+          width: 1,
+          color: Colors.black,
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: const BorderSide(
+          width: 1,
+          color: Colors.black,
+        ),
+      ),
+    ),
+  );
+}
